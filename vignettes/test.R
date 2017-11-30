cfa5a44adf4e17c29ab11c18d4011e4fc8deec04
Volker Schmid <volker.schmid@lmu.de>
2017-11-30 12:24:53 +0100

    simulation vignette more bugs to fix

--- vignettes/test.R
+++ vignettes/test.R
@@ -1,12 +1,7 @@
-setwd("~/Dropbox/temp/bamp/")
+
 library(bamp)
 data(apc)
-model1 <- bamp(cases, population, age="rw1", period="rw1", cohort="rw1",
-               periods_per_agegroup = 5,  verbose=TRUE)
 
-print(model1)
-samples<-model1$samples
-
-model2 <- bamp(cases, population, age="rw2", period="rw2", cohort="rw2",
+model <- bamp(cases, population, age="rw1", period="rw1", cohort="rw1", overdisp = TRUE,
                periods_per_agegroup = 5,  verbose=TRUE)
 
