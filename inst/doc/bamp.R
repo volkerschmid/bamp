d9dd076f246a1aa7ac5556e044704ff149c8d407
Volker Schmid <volker.schmid@lmu.de>
2018-09-14 16:24:48 +0200

    .

---
+++ inst/doc/bamp.R
@@ -0,0 +1,40 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, cache=TRUE)
+library(bamp)
+
+## ----loadplot------------------------------------------------------------
+data(apc)
+plot(cases[,1],type="l",ylim=range(cases), ylab="cases", xlab="year", main="cases per age group")
+for (i in 2:8)lines(cases[,i], col=i)
+
+## ----Model1_with_RW1_priors----------------------------------------------
+model1 <- bamp(cases, population, age="rw1", period="rw1", cohort="rw1",
+              periods_per_agegroup = 5)
+
+## ----check_model---------------------------------------------------------
+checkConvergence(model1)
+
+## ----print_model---------------------------------------------------------
+print(model1)
+
+## ----plot_model----------------------------------------------------------
+plot(model1)
+
+## ----plot_model_with_more_quantiles--------------------------------------
+plot(model1, quantiles = c(0.025,0.1,0.5,0.9,0.975))
+
+## ----predmodel-----------------------------------------------------------
+model2 <- bamp(cases[-10,], population[-10,], age="rw1", period="rw1", cohort="rw1",
+              periods_per_agegroup = 5)
+
+## ----prediction----------------------------------------------------------
+prediction<-predict_apc(model2, periods=1, population=population)
+
+## ------------------------------------------------------------------------
+ts.plot(t(prediction$period), lty=c(2,1,2))
+ts.plot(t(prediction$cohort), lty=c(2,1,2))
+
+## ------------------------------------------------------------------------
+ts.plot(t(prediction$cases_period), lty=c(2,1,2))
+points(apply(cases,1,sum), pch=19)
+
