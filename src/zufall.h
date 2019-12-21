3b4c6d60a4dcf5eef83610b96a26d2bbb8dd022d
schmid <schmid@localhost.localdomain>
2019-12-21 16:59:39 +0100

    fix warnings

--- src/zufall.h
+++ src/zufall.h
@@ -4,61 +4,7 @@
 //Erzeugt gleichverteilte Zufallszahlen auf [0,1]
 double nulleins();
 
-
-
-
-//Erzeugt diskrete gleichverteilte Zufallszahlen auf 1,...,k
-
-//int einsk(int k);
-
-
-//Erzeugt diskrete gleichverteilte Zufallszahlen auf 1,...,k
-//gibt double zurueck
-//double d_einsk(int k);
-
-
-//Erzeugt diskrete gleichverteilte Zufallszahlen auf 0,...,k
-
-//int nullk(int k);
-
-
-//Erzeugt gleichverteilte Zufallszahlen auf [0,L]
-
-//double nullell(double l);
-
-
-//Erzeugt gleichverteilte Zufallszahlen auf [a,b]
-
-//double gleichab(double a, double b);
-
-
-//Erzeugt gleichverteilte Zufallszahlen auf [-1,1]
-
-//double reins();
-
-
-
-//Erzeugt gleichverteilte Zufallszahlen auf [-0.5,0.5]
-
-//double halbeins();
-
-
-//Erzeugt exonentialverteilte Zufallszahlen
-
-//double expo(double lambda);
-
-
-
-//Erzeugt poissonverteilte Zufallszahlen
-
-//int poisson(double lambda);
-
-
-
 //Erzeugt normalverteilte Zufallszahlen
-
-
-//double zahl(double mu, double sigma);
 double normal(double mu, double sigma);
 
 
@@ -66,4 +12,3 @@ double normal(double mu, double sigma);
 double RNDGAM(double a, double b);
 
 //int rbinom(double p,int n);
-  
